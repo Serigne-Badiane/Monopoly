3c78421757d9d70a4d541e6334b36b0564a1ff36
monopolypo <coca90000@gmail.com>
2022-01-02 15:07:47 +0100

    plateau (bug encore je crois)

--- main.c
+++ main.c
@@ -13,6 +13,19 @@ void Color(int couleurDuTexte,int couleurDeFond)
     SetConsoleTextAttribute(H,couleurDeFond*16+couleurDuTexte);
 }
 
+void gotoligcol( int lig, int col )
+{
+
+COORD mycoord;
+
+mycoord.X = col;
+
+mycoord.Y = lig;
+
+SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), mycoord );
+
+}
+
 void pause()
 {
     char pause=kbhit('p');
@@ -21,6 +34,255 @@ void pause()
     {
         menu1();
     }
+}
+
+void plateau()
+{
+    ///premiere ligne de case
+    for(int i;i<8;i++)
+    {
+    printf(" ______ ");
+    }
+    printf("\n");
+
+    for(int i=0;i<2;i++)
+    {
+    for(int k=0;k<8;k++)
+    {
+        printf("|      |");
+    }
+    printf("\n");
+    if (i==1)
+    {
+        for(int j=0;j<8;j++)
+            {
+            printf("|______|");
+            }
+    }
+    }
+    printf("\n");
+
+
+    //||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+    /// toutes les lignes de case du milieu appart l'avant dernière
+    for (int i=0;i<5;i++)
+    {
+    for (int i=0;i<2;i++)
+    {
+    printf("|      |");
+    for(int i=0;i<6;i++)
+    {
+        printf("        ");
+    }
+    printf("|      |\n");
+    }
+    printf("|______|");
+    for(int i=0;i<6;i++)
+    {
+        printf("        ");
+    }
+    printf("|______|\n");
+    }
+
+
+
+
+    ///avant derniere ligne de case
+
+    for (int i=0;i<2;i++)
+    {
+    printf("|      |");
+    for(int i=0;i<6;i++)
+    {
+        printf("        ");
+    }
+    printf("|      |\n");
+    }
+    printf("|______|");
+    for(int i=0;i<6;i++)
+    {
+        printf(" ______ ");
+    }
+    printf("|______|\n");
+
+
+
+
+    //||||||||||||||||||||||||-
+
+
+    /*printf("        ");
+    for(int i;i<6;i++)
+    {
+    printf(" ______ ");
+    }
+    printf("        ");
+    printf("\n");*/
+
+
+    ///derniere ligne de case
+    for(int i=0;i<2;i++)
+    {
+    for(int k=0;k<8;k++)
+    {
+        printf("|      |");
+    }
+    printf("\n");
+    if (i==1)
+    {
+        for(int j=0;j<8;j++)
+            {
+            printf("|______|");
+            }
+    }
+    }
+    printf("\n");
+
+
+
+    gotoligcol(2,1);
+    printf("DEPART");
+    gotoligcol(3,3);
+    Color(4,0);
+    printf("%c",0x1A);
+
+    Color(6,0);
+    gotoligcol(3,9);
+    printf("LUNE");
+    Color(15,0);
+    printf("%c",0xB8);
+
+    Color(6,0);
+    gotoligcol(3,17);
+    printf("TERRE");
+
+    Color(15,0);
+    gotoligcol(3,25);
+    printf("TAXE%c",0x24);
+
+    Color(0,15);
+    gotoligcol(2,33);
+    printf("VOIE  ");
+    gotoligcol(3,33);
+    printf("LACTEE");
+
+    Color(11,0);
+    gotoligcol(3,41);
+    printf("PHOBOS");
+
+    gotoligcol(3,50);
+    printf("MARS");
+
+    Color(15,0);
+    gotoligcol(2,59);
+    printf("GO");
+    gotoligcol(3,57);
+    printf("PRISON");
+    gotoligcol(4,59);
+    Color(6,0);
+    printf("%c%c",0xB2,0xB2);
+
+    ///2ème ligne
+    gotoligcol(6,1);
+    printf("CHANCE");
+    Color(13,0);
+    gotoligcol(6,57);
+    printf("GARYME");
+    ///3ème ligne
+    Color(14,0);
+    gotoligcol(8,3);
+    printf("%c",0x0F);
+    Color(9,0);
+    gotoligcol(9,1);
+    printf("SOLEIL");
+    Color(13,0);
+    gotoligcol(9,57);
+    printf("CALLIS");
+    ///4ème ligne
+    Color(9,0);
+    gotoligcol(12,1);
+    printf("MERCUR");
+    Color(0,15);
+    gotoligcol(11,57);
+    printf("ANDRO ");
+    gotoligcol(12,57);
+    printf(" MEDE ");
+    ///5ème ligne
+    Color(0,15);
+    gotoligcol(14,1);
+    printf("NUAGE ");
+    gotoligcol(15,1);
+    printf("MAGELA");
+    Color(6,0);
+    gotoligcol(15,59);
+    printf("IO");
+    ///6ème ligne
+    Color(2,0);
+    gotoligcol(18,1);
+    printf("URANUS");
+    Color(6,0);
+    gotoligcol(17,58);
+    printf("JUPI");
+    gotoligcol(18,58);
+    printf("TER");
+    ///7ème ligne
+    Color(2,0);
+    gotoligcol(21,1);
+    printf("VENUS");
+    Color(1,0);
+    gotoligcol(20,57);
+    printf("COMMU");
+    gotoligcol(21,57);
+    printf("NNOTE");
+    ///8ème ligne
+    Color(9,0);
+    gotoligcol(24,1);
+    printf("PRISON");
+    gotoligcol(25,3);
+    Color(6,0);
+    printf("%c%c",0xB2,0xB2);
+
+    Color(1,0);
+    gotoligcol(23,9);
+    printf("COMMU");
+    gotoligcol(24,9);
+    printf("NNOTE");
+
+    Color(14,0);
+    gotoligcol(23,18);
+    printf("NEP");
+    gotoligcol(24,18);
+    printf("TUNE");
+
+    gotoligcol(24,25);
+    printf("PLUTON");
+
+    Color(0,15);
+    gotoligcol(23,33);
+    printf("VOIE  ");
+    gotoligcol(24,33);
+    printf("TETARD");
+
+    Color(4,0);
+    gotoligcol(23,42);
+    printf("SAT");
+    gotoligcol(24,42);
+    printf("URNE");
+
+    gotoligcol(24,49);
+    printf("TITAN");
+
+    Color(12,0);
+    gotoligcol(23,58);
+    printf("PAR");
+    gotoligcol(24,58);
+    printf("KING");
+
+
+
+    Color(15,0);
+    gotoligcol(30,0);
 }
 
 int main()
